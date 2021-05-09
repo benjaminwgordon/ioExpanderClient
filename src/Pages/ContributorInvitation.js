49f4027b8dee61fd53b2b8d094d80d354fb196d5
Ben Gordon <Bengordon1995@gmail.com>
2021-05-09 16:39:45 -0500

    rebuilt to use query params instead of props to facilitate linking from multiple places in appliaction

--- src/Pages/ContributorInvitation.js
+++ src/Pages/ContributorInvitation.js
@@ -2,29 +2,39 @@ import React, {useState, useEffect, useContext} from 'react'
 import authenticationContext from '../authenticationContext'
 import query from '../query'
 import Page from '../Components/Page'
+import {useLocation} from 'react-router-dom'
 
-const ContributorInvitation = (props) => {
+const ContributorInvitation = () => {
+
+    function useQuery() {
+        return new URLSearchParams(useLocation().search);
+      }
 
     const context = useContext(authenticationContext)
     const token = context.user.token
     const user_id = context.user.user_id
+    const queryParams = useQuery()
     const [targetUser, setTargetUser] = useState(null)
     const [targetProject, setTargetProject] = useState(null)
 
     useEffect(()=>{
         // Handle optional presets for user id and project id
-        if (props.targetUser){
-            setTargetUser(props.targetUser)
+        console.log({queryParams})
+        if (queryParams.get("user_id")){
+            setTargetUser(queryParams.get("user_id"))
         }
-        if (props.targetProject){
-            setTargetProject(props.targetProject)
+        if (queryParams.get("project_id")){
+            setTargetProject(queryParams.get("project_id"))
         }
     }, [])
 
     useEffect(()=>{
         const fetchOwnedProjects = async () => {
             const res = await query.get(`/projects?user_id=${user_id}`, token)
+            const ownedProjects = res
+            console.log({ownedProjects})
         }        
+        fetchOwnedProjects()
     },[targetUser, targetProject])
 
 
